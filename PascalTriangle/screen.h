872be20591d2d7d73e8a28d79ff1d47c95a19240
e2302312 <e2302312@WA2029-18-OLD>
2024-04-03 15:00:55 +0300

    Add project files.

---
+++ PascalTriangle/screen.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//define all the color as constants
+enum COLORS {BLACK=30, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE };
+
+
+// function declerations
+void setFGcolor(int);
+void clearScreen(void);
+void gotoXY(int, int);
